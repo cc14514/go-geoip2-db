7b6408a9812a30eb8a355c0413bfa88836f65459
liangchuan <cc14514@icloud.com>
2019-01-06 14:31:42 +0800

    fix unit test

--- factory_test.go
+++ factory_test.go
@@ -1,7 +1,6 @@
 package geoip2db
 
 import (
-	"fmt"
 	"net"
 	"testing"
 )
@@ -9,11 +8,12 @@ import (
 func TestNewGeoipDbByStatik(t *testing.T) {
 	db, _ := NewGeoipDbByStatik()
 	defer db.Close()
-	record, _ := db.City(net.ParseIP("115.35.95.90"))
-	fmt.Printf("Portuguese (BR) city name: %v\n", record.City.Names["pt-BR"])
-	fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
-	fmt.Printf("Russian country name: %v\n", record.Country.Names["ru"])
-	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
-	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
-	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
+	record, _ := db.City(net.ParseIP("47.104.232.24"))
+	//record, _ := db.City(net.ParseIP("47.100.215.147"))
+	t.Logf("Portuguese (BR) city name: %v\n", record.City.Names)
+	t.Logf("English subdivision name: %v\n", record.Subdivisions)
+	t.Logf("Russian country name: %v\n", record.Country)
+	t.Logf("ISO country code: %v\n", record.Country.IsoCode)
+	t.Logf("Time zone: %v\n", record.Location.TimeZone)
+	t.Logf("Location : %v, %v\n", record.Location.Latitude, record.Location.Longitude)
 }
